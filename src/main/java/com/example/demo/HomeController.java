082ed344528551145c822bf839243d5e741b5757
jitendra kumar <jitendrakv11@gmail.com>
2022-02-18 21:19:26 +0530

    first commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	
+	@GetMapping(value ="/home")
+	public String HomePage() {
+		return "this is my website";
+	}
+	
+
+}
